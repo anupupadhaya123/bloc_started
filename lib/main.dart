c97060f48697fede21662428cbca52436e18d466
Anup Upadhaya <au.anupupadhaya@gmail.com>
2023-01-22 15:12:21 +0545

    Checking connection using cubit without event

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bloc_app/bloc/internet_bloc/internet_bloc.dart';
+import 'package:bloc_app/cubit/internet_cubit.dart';
 import 'package:bloc_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => InternetBloc(),
+      create: (context) => InternetCubit(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
-import 'package:bloc_app/bloc/internet_bloc/internet_bloc.dart';
-import 'package:bloc_app/bloc/internet_bloc/internet_state.dart';
+import 'package:bloc_app/cubit/internet_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,14 +10,14 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: BlocConsumer<InternetBloc, InternetState>(
+          child: BlocConsumer<InternetCubit, InternetState>(
             listener: (context, state) {
-              if (state is InternetGainedState) {
+              if (state == InternetState.Gained) {
                 ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                   content: Text("Internet Connecteed"),
                   backgroundColor: Colors.green,
                 ));
-              } else if (state is InternetLostState) {
+              } else if (state == InternetState.Lost) {
                 ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
                   content: Text("Internet not"),
                   backgroundColor: Colors.red,
@@ -26,9 +25,9 @@ class HomeScreen extends StatelessWidget {
               }
             },
             builder: (context, state) {
-              if (state is InternetGainedState) {
+              if (state == InternetState.Gained) {
                 return const Text("Connected");
-              } else if (state is InternetLostState) {
+              } else if (state == InternetState.Lost) {
                 return const Text("Not Connected");
               } else {
                 return const Text("Loading.");
